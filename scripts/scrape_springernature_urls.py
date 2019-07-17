922dcc49d4dff71aa5dc441e1e1020d073077e87
Amy Jiang <52675002+p-amyjiang@users.noreply.github.com>
2019-07-17 13:55:28 -0700

    Scrapes springer nature publications

--- scripts/scrape_springernature_urls.py
+++ scripts/scrape_springernature_urls.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 import time
 
 """
-This code is used to scrape ACS of publication urls and write them to
+This code is used to scrape Springer Nature of publication urls and write them to
 a text file in the current directory for later use.
 
 """
@@ -30,7 +30,8 @@ def clean(elems):
     urls = []
     for elem in elems:
         url = elem.get_attribute("href")
-        if 'doi' in url and 'pdf' not in url\
+        if 'article' in url or 'chapter' in url\
+                            and 'pdf' not in url\
                             and 'search' not in url\
                             and 'abs' not in url\
                             and 'full' not in url\
@@ -40,22 +41,22 @@ def clean(elems):
 
 def build_annual_urls(search_term,max_pages):
     """
-    This method takes the first ACS url and creates a list of
-    urls which lead to the following pages on ACS which will be
+    This method takes the first Springer Nature url and creates a list of
+    urls which lead to the following pages on Springer Nature which will be
     scraped. 
     """
-    first_url = 'https://pubs.acs.org/action/doSearch?AllField=' + search_term.replace(" ", "+") + '&startPage=0&pageSize=100'
+    first_url = 'https://link.springer.com/search/page/1?query=' + search_term.replace(" ", "+") + '&startPage=0&pageSize=100'
     
     page_urls = [first_url]
-    for i in range(max_pages-1):
-        urli = first_url.replace("startPage=0", "startPage=" + str(i))
+    for i in range(2,max_pages):
+        urli = first_url.replace("page/1", "page/" + str(i))
         page_urls.append(urli)
 
     return page_urls
 
 def scrape_all(first_url,driver,max_pages):
     """
-    This method takes the first ACS url and navigates
+    This method takes the first Springer Nature url and navigates
     through all pages of listed publications, scraping each url
     on each page. Returns a list of the urls.
     """
@@ -83,7 +84,7 @@ def proxify(scraped_urls,prefix):
 
     proxy_urls = []
     for url in scraped_urls:
-        sd_id = url[21:]
+        sd_id = url[26:]
         newlink = prefix + sd_id
         proxy_urls.append(newlink)
 
@@ -100,14 +101,14 @@ def write_urls(urls,filename):
 
 
 
-driver = webdriver.Chrome()
-prefix = 'https://pubs-acs-org.offcampus.lib.washington.edu/'
-
-search_term = input("Input ACS search term: ")
+search_term = input("Input Springer Nature search term: ")
 print('\n')
 filename = input("Input filename with .txt extension you wish to store urls in: ")
 print('\n')
 max_pages = int(input("Input maximum number of pages to scrape: "))
+
+driver = webdriver.Chrome()
+prefix = 'https://link-springer-com.offcampus.lib.washington.edu/'
 
 master_list = []
 
